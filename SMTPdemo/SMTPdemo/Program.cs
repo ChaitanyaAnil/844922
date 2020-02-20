2204f25c6a7386e79fbb5b0ee789aeb816b68343
ChaitanyaAnil <60813901+ChaitanyaAnil@users.noreply.github.com>
2020-02-20 16:54:29 +0530

    commit

---
+++ SMTPdemo/SMTPdemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+namespace SMTPdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fromAddress = new MailAddress("chaitanyaanil97@gmail.com", "from");
+            var toAddress = new MailAddress("sasidurgaprasad@gmail.com", "to");
+            const string fromPassword = "******";
+            const string subject = "subjct";
+            const string body = "Body --sampleemail sending through smtp";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+        }
+    }
+}
